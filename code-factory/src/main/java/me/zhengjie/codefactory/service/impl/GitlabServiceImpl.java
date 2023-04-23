f6a788ce25ae5ece1354cc0574e4adf31912a233
shenjizhe <shenjizhe@bfec.cn>
2023-04-23 11:09:30 +0800

    1. 推送内存中的内容到 gitlab

--- code-factory/src/main/java/me/zhengjie/codefactory/service/impl/GitlabServiceImpl.java
+++ code-factory/src/main/java/me/zhengjie/codefactory/service/impl/GitlabServiceImpl.java
@@ -13,10 +13,7 @@ import me.zhengjie.codefactory.service.agent.CodeFactoryAgent;
 import me.zhengjie.codefactory.service.agent.CodeOutput;
 import me.zhengjie.codefactory.service.agent.CodeOutputItem;
 import org.gitlab4j.api.*;
-import org.gitlab4j.api.models.Commit;
-import org.gitlab4j.api.models.CommitAction;
-import org.gitlab4j.api.models.Project;
-import org.gitlab4j.api.models.SshKey;
+import org.gitlab4j.api.models.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -73,8 +70,11 @@ public class GitlabServiceImpl implements GitlabService {
     @Override
     public Result createProject(Long componentId) {
         final Component component = componentRepository.getById(componentId);
+        Namespace namespace = new Namespace();
+//        namespace.setName(Const.ConfigKey.GIT_NAMESPACE);
         Project projectSettings = new Project()
                 .withName(component.getName())
+//                .withNamespace(namespace)
                 .withDescription(component.getComment())
                 .withIssuesEnabled(true)
                 .withMergeRequestsEnabled(true)
@@ -89,34 +89,23 @@ public class GitlabServiceImpl implements GitlabService {
         }
     }
 
-//    private void commitFile(Project project, ArrayList actions) {
-//        // Make sure the file to create does not exist.
-//        if (gitLabApi.getRepositoryFileApi().getOptionalFile(project, path, "master").isPresent()) {
-//            try {
-//                gitLabApi.getRepositoryFileApi().deleteFile(project, path, "master", content);
-//            } catch (GitLabApiException ignore) {
-//            }
-//        }
-//
-//        CommitAction commitAction = new CommitAction()
-//                .withAction(CommitAction.Action.CREATE)
-//                .withContent("This is the original data in the file")
-//                .withFilePath(path);
-//
-//        Commit commit = gitLabApi.getCommitsApi().createCommit(
-//                project, "master", "code factory init", null, null, null, actions);
-//
-//
-//    }
-
     @Override
     public Result pushCode(Long componentId) {
         final Component component = componentRepository.getById(componentId);
         final ProjectApi projectApi = gitLabApi.getProjectApi();
         final CommitsApi commitsApi = gitLabApi.getCommitsApi();
         try {
-            Project project = projectApi.getProject(Const.Name.SSH_TITLE, component.getName());
-            Long projectId = project.getId();
+            final String name = component.getName();
+            final List<Project> projects = projectApi.getProjects();
+            Project project = null;
+            for (Project pro : projects) {
+                final String pname = pro.getNamespace().getName();
+                if ("gitlab".equals(pname) || name.equals(pro.getName())) {
+                    project = pro;
+                    break;
+                }
+            }
+//            Project project = projectApi.getProject("gitlab", name);
 
             final String generate = agent.generate(12L);
             final CodeOutput output = agent.output(generate);
@@ -126,17 +115,25 @@ public class GitlabServiceImpl implements GitlabService {
 
             for (String uuid : items) {
                 CodeOutputItem item = agent.outputItem(uuid);
-                actions.add(
-                        new CommitAction()
-                                .withFilePath(item.getFullPath())
-                                .withContent(item.getCode()));
-
-                //TODO: 推送代码逻辑，参考 https://github.com/gitlab4j/gitlab4j-api/blob/main/src/test/java/org/gitlab4j/api/TestCommitsApi.java
-//                commitsApi.
+                if (item.getSavePath() != null) {
+                    actions.add(
+                            new CommitAction()
+                                    .withAction(CommitAction.Action.CREATE)
+                                    .withFilePath(item.getFullPath())
+                                    .withContent(item.getCode()));
+                }
             }
+            Commit commit = gitLabApi.getCommitsApi().createCommit(
+                    project,
+                    "master",
+                    "initial",
+                    null,
+                    null,
+                    null,
+                    actions);
 
 
-            return Result.fail(ErrorCode.Fail);
+            return Result.success(commit);
         } catch (GitLabApiException e) {
             e.printStackTrace();
             return Result.exception(ErrorCode.GitlabError, e);
