0c3f1a5cd88eb9d94e8f5d4bde26895786f92d2c
shenjizhe <shenjizhe@bfec.cn>
2023-04-25 11:38:04 +0800

    1. 测试时，hardcode 了 组件ID

--- code-factory/src/main/java/me/zhengjie/codefactory/service/ConfigService.java
+++ code-factory/src/main/java/me/zhengjie/codefactory/service/ConfigService.java
@@ -1,83 +1,99 @@
 /*
-*  Copyright 2019-2020 Jason Shen
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*/
+ *  Copyright 2019-2020 Jason Shen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package me.zhengjie.codefactory.service;
 
 import me.zhengjie.codefactory.domain.Config;
 import me.zhengjie.codefactory.service.dto.ConfigDto;
 import me.zhengjie.codefactory.service.dto.ConfigQueryCriteria;
 import org.springframework.data.domain.Pageable;
-import java.util.Map;
-import java.util.List;
-import java.io.IOException;
+
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
-* @website https://eladmin.vip
-* @description 服务接口
-* @author Jason Shen
-* @date 2023-04-11
-**/
+ * @author Jason Shen
+ * @website https://eladmin.vip
+ * @description 服务接口
+ * @date 2023-04-11
+ **/
 public interface ConfigService {
 
     /**
-    * 查询数据分页
-    * @param criteria 条件
-    * @param pageable 分页参数
-    * @return Map<String,Object>
-    */
-    Map<String,Object> queryAll(ConfigQueryCriteria criteria, Pageable pageable);
+     * 查询数据分页
+     *
+     * @param criteria 条件
+     * @param pageable 分页参数
+     * @return Map<String, Object>
+     */
+    Map<String, Object> queryAll(ConfigQueryCriteria criteria, Pageable pageable);
 
     /**
-    * 查询所有数据不分页
-    * @param criteria 条件参数
-    * @return List<ConfigDto>
-    */
+     * 查询所有数据不分页
+     *
+     * @param criteria 条件参数
+     * @return List<ConfigDto>
+     */
     List<ConfigDto> queryAll(ConfigQueryCriteria criteria);
 
     /**
      * 根据ID查询
+     *
      * @param id ID
      * @return ConfigDto
      */
     ConfigDto findById(Long id);
 
     /**
-    * 创建
-    * @param resources /
-    * @return ConfigDto
-    */
+     * 根据 key 返回值
+     *
+     * @param key 关键字
+     * @return 值（根据类型进行了转换）
+     */
+    Object getByKey(String key);
+
+    /**
+     * 创建
+     *
+     * @param resources /
+     * @return ConfigDto
+     */
     ConfigDto create(Config resources);
 
     /**
-    * 编辑
-    * @param resources /
-    */
+     * 编辑
+     *
+     * @param resources /
+     */
     void update(Config resources);
 
     /**
-    * 多选删除
-    * @param ids /
-    */
+     * 多选删除
+     *
+     * @param ids /
+     */
     void deleteAll(Long[] ids);
 
     /**
-    * 导出数据
-    * @param all 待导出的数据
-    * @param response /
-    * @throws IOException /
-    */
+     * 导出数据
+     *
+     * @param all      待导出的数据
+     * @param response /
+     * @throws IOException /
+     */
     void download(List<ConfigDto> all, HttpServletResponse response) throws IOException;
 }
---
+++ code-factory/src/test/java/me/zhengjie/codefactory/service/impl/CICDServiceImplTest.java
@@ -0,0 +1,50 @@
+package me.zhengjie.codefactory.service.impl;
+
+import me.zhengjie.base.Result;
+import me.zhengjie.codefactory.service.CICDService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * CICDServiceImpl Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>4月 24, 2023</pre>
+ */
+@SpringBootTest
+public class CICDServiceImplTest {
+    @Autowired
+//    private CICDServiceImpl cicdService;
+
+//    @BeforeAll
+//    public void before() throws Exception {
+//    }
+//
+//    @AfterAll
+//    public void after() throws Exception {
+//    }
+
+    /**
+     * Method: downloadComponent(Long componentId)
+     */
+    @Test
+    public void testDownloadComponent() throws Exception {
+//TODO: Test goes here...
+//        final Result result = cicdService.downloadComponent(12L);
+    }
+
+
+    /**
+     * Method: init()
+     */
+    @Test
+    public void testInit() throws Exception {
+//TODO: Test goes here... 
+
+    }
+
+} 
