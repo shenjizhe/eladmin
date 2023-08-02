a33ca78dac2edc4f381c56867f93155fc2eb821d
shenjizhe <shenjizhe@bfec.cn>
2023-08-02 15:59:16 +0800

    1. 更新了v1.0.0版本对应的数据库脚本

--- morpheme/src/main/java/me/zhengjie/morpheme/rest/MorphemeStudyController.java
+++ morpheme/src/main/java/me/zhengjie/morpheme/rest/MorphemeStudyController.java
@@ -2,20 +2,15 @@ package me.zhengjie.morpheme.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import javafx.util.Pair;
 import lombok.RequiredArgsConstructor;
 import me.zhengjie.annotation.Log;
 import me.zhengjie.morpheme.domain.MorphemePair;
-import me.zhengjie.morpheme.domain.MorphemeStudy;
-import me.zhengjie.morpheme.domain.Word;
 import me.zhengjie.morpheme.service.MorphemeStudyService;
 import me.zhengjie.utils.SecurityUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.PostConstruct;
 
 @RestController
 @RequiredArgsConstructor
